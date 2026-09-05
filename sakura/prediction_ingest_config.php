fe909b774b1ce5907d152c4df483dfee687b723d
Joker7822 <lsakazu@yahoo.co.jp>
2026-09-06 08:15:06 +0900

    Delete sakura/prediction_ingest_config.php

--- sakura/prediction_ingest_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Copy to prediction_ingest_config.php on Sakura. Do not commit the real file.
-return [
-    'db_dsn' => 'mysql:host=mysql3102.db.sakura.ne.jp;dbname=shinnagoya_loto7;charset=utf8mb4',
-    'db_user' => 'ashinnagoya_loto7',
-    'db_password' => 'araki0401',
-    'hmac_secret' => 'araki0401',
-    'max_clock_skew_seconds' => 300,
-];
